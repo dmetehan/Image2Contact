2feaf0ec82870d814abe1bcb76b49f64d7b5ac7f
M.Doyran <m.doyran@uu.nl>
2024-03-25 14:52:33 +0100

    grahping results

--- YOUth_train.py
+++ YOUth_train.py
@@ -10,7 +10,7 @@ from sklearn.metrics import accuracy_score, f1_score, jaccard_score
 
 from argparse import ArgumentParser
 from ContactSignatureModel import ContactSignatureModel, initialize_model
-from utils import parse_config, get_experiment_name, find_last_values_tensorboard
+from utils import parse_config, get_experiment_name, find_last_values_tensorboard, vis_pred_errors_heatmap, vis_threshold_eval
 
 # this is important for FLickrCI3DClassification. It doesn't allow importing v2 after initializing the network.
 import torchvision.transforms.v2 as transforms
@@ -38,7 +38,7 @@ class EarlyStopping:
                 self.early_stop = True
 
 
-def train_one_epoch(model, optimizer, loss_fn, loss_weights, train_loader, epoch_index, tb_writer, batch_size, multitask=True):
+def train_one_epoch(model, optimizer, loss_fn, loss_weights, train_loader, epoch_index, tb_writer, batch_size, save_dir, multitask=True):
     overall_loss = 0.
     running_loss = 0.
     last_loss = 0.
@@ -47,14 +47,16 @@ def train_one_epoch(model, optimizer, loss_fn, loss_weights, train_loader, epoch
     # iter(training_loader) so that we can track the batch
     # index and do some intra-epoch reporting
     i = 0
-    all_preds = {key: np.zeros((len(train_loader.dataset), eval(key), batch_size)) for key in model.output_keys}
-    all_labels = {key: np.zeros((len(train_loader.dataset), eval(key), batch_size)) for key in model.output_keys}
+    pred_scores = {key: [] for key in model.output_keys}
+    all_labels = {key: [] for key in model.output_keys}
+    all_preds = {key: [] for key in model.output_keys}
     for i, data in enumerate(train_loader):
         # Every data instance is an input + label pair
         _, inputs, labels = data
         for key in all_labels:
-            all_labels[key][i, :, :len(labels[key])] = labels[key].T
             labels[key] = labels[key].to(device)
+            all_labels[key] += labels[key].detach().cpu().tolist()
+
         inputs = inputs.to(device)
         # Zero your gradients for every batch!
         optimizer.zero_grad()
@@ -63,10 +65,8 @@ def train_one_epoch(model, optimizer, loss_fn, loss_weights, train_loader, epoch
         outputs_list = model(inputs)
         loss = torch.zeros(1).to(device)
         for k, key in enumerate(labels):
-            preds = outputs_list[k].T.detach().cpu()
-            preds[preds >= model.thresholds[key]] = 1
-            preds[preds < model.thresholds[key]] = 0
-            all_preds[key][i, :, :len(labels[key])] = preds
+            pred_scores[key] += outputs_list[k].detach().cpu().tolist()
+            all_preds[key] += (torch.sigmoid(outputs_list[k].detach().cpu()) > model.thresholds[key]).long().tolist()
             # Compute the loss and its gradients
             loss += loss_weights[key] * loss_fn(outputs_list[k], labels[key].float())
             #print(key, loss)
@@ -87,9 +87,10 @@ def train_one_epoch(model, optimizer, loss_fn, loss_weights, train_loader, epoch
             running_loss = 0.
     jaccard = {}
     for key in all_labels:
-        all_preds[key] = np.swapaxes(all_preds[key], 1, 2).reshape(-1, eval(key))[:-(batch_size - len(labels[key])), :]
-        all_labels[key] = np.swapaxes(all_labels[key], 1, 2).reshape(-1, eval(key))[:-(batch_size - len(labels[key])), :]
         jaccard[key] = jaccard_score(all_labels[key], all_preds[key], average='micro', zero_division=0)
+    # Visualize thresholding graphs
+    kwargs = {'epoch': epoch_index, 'save_dir': os.path.join(save_dir, 'thresholding', 'train'), 'average': 'micro'}
+    vis_threshold_eval(all_labels, pred_scores, jaccard_score, **kwargs)
     # acc = accuracy_score(all_labels, all_preds)
     # f1 = f1_score(all_labels, all_preds, average="micro")
     if i % 1000 != 999:
@@ -123,45 +124,45 @@ def train_model(model, optimizer, scheduler, loss_fn_train, experiment_name, cfg
         best_vloss = 1_000_000.
         best_vacc_blncd = 0.0
     writer = SummaryWriter('{}/{}_{}'.format(exp_dir, experiment_name, timestamp))
-
+    save_dir = os.path.join(exp_dir, experiment_name, timestamp)
     model = model.to(device)
     for epoch in range(start_epoch, cfg.EPOCHS):
         print('EPOCH {}:'.format(epoch + 1))
 
         # Make sure gradient tracking is on, and do a pass over the data
         model.train(True)
-        avg_loss, jaccard = train_one_epoch(model, optimizer, loss_fn_train, loss_weights, train_loader, epoch, writer, cfg.BATCH_SIZE)
+        avg_loss, jaccard = train_one_epoch(model, optimizer, loss_fn_train, loss_weights, train_loader, epoch, writer, cfg.BATCH_SIZE, save_dir)
 
         # We don't need gradients on to do reporting
         model.train(False)
 
         running_vloss = 0.0
         i = 0
-        all_preds = {key: np.zeros((len(val_loader.dataset), eval(key), cfg.BATCH_SIZE)) for key in model.output_keys}
-        all_labels = {key: np.zeros((len(val_loader.dataset), eval(key), cfg.BATCH_SIZE)) for key in model.output_keys}
+        pred_scores = {key: [] for key in model.output_keys}
+        all_labels = {key: [] for key in model.output_keys}
+        all_preds = {key: [] for key in model.output_keys}
         with torch.no_grad():
             for i, vdata in enumerate(val_loader):
                 _, vinputs, vlabels = vdata
                 for key in all_labels:
-                    all_labels[key][i, :, :len(vlabels[key])] = vlabels[key].T
                     vlabels[key] = vlabels[key].to(device)
+                    all_labels[key] += vlabels[key].detach().cpu().tolist()
                 vinputs = vinputs.to(device)
 
                 vloss = torch.zeros(1).to(device)
                 voutputs_list = model(vinputs)
                 for k, key in enumerate(vlabels):
-                    vpreds = voutputs_list[k].T.detach().cpu()
-                    vpreds[vpreds >= model.thresholds[key]] = 1
-                    vpreds[vpreds < model.thresholds[key]] = 0
-                    all_preds[key][i, :, :len(vlabels[key])] = vpreds
+                    pred_scores[key] += voutputs_list[k].detach().cpu().tolist()
+                    all_preds[key] += (torch.sigmoid(voutputs_list[k].detach().cpu()) > model.thresholds[key]).long().tolist()
+                    # Compute the loss and its gradients
                     vloss += loss_weights[key] * loss_fn_valid(voutputs_list[k], vlabels[key].float())
                 running_vloss += vloss.detach().item()
 
         vjaccard = {}
         for key in all_labels:
-            all_preds[key] = np.swapaxes(all_preds[key], 1, 2).reshape(-1, eval(key))[:-(cfg.BATCH_SIZE - len(vlabels[key])), :]
-            all_labels[key] = np.swapaxes(all_labels[key], 1, 2).reshape(-1, eval(key))[:-(cfg.BATCH_SIZE - len(vlabels[key])), :]
             vjaccard[key] = jaccard_score(all_labels[key], all_preds[key], average='micro', zero_division=0)
+        kwargs = {'epoch': epoch, 'save_dir': os.path.join(save_dir, 'thresholding', 'val'), 'average': 'micro'}
+        vis_threshold_eval(all_labels, pred_scores, jaccard_score, **kwargs)
         avg_vloss = running_vloss / (i + 1)
         scheduler.step(avg_vloss)
         print('LOSS train {:.4f} valid {:.4f} - Jaccard train {} valid {}'.format(avg_loss,
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import os
 import cv2
 import glob
 
+import torch
+import matplotlib.pyplot as plt
 import numpy as np
 import yaml
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
@@ -134,3 +136,65 @@ def get_adj_mat():
     A = A / np.sum(A, axis=0)
     assert np.array_equal(np.sum(A, axis=0), np.ones((A.shape[0])))
     return A
+
+
+def _init_heatmaps():
+    people = ['adult', 'child']
+    sketches = {pers: cv2.imread('data/rid_base.png') for pers in people}
+
+    def create_mask_index():
+        mask_ind = {pers: np.zeros_like(cv2.imread(f'data/rid_base.png', 0)) for pers in people}
+        for pers in people:
+            for rid in range(21):
+                mask = cv2.imread(f'data/masks_coarse_clear/rid_{rid}.png', 0)
+                mask_ind[pers][mask < 100] = rid + 1
+        return mask_ind
+
+    return people, sketches, create_mask_index()
+
+
+def vis_pred_errors_heatmap(gts, preds, save_dir):
+    os.makedirs(save_dir, exist_ok=True)
+    gts = torch.Tensor(gts) == 1
+    preds = torch.Tensor(preds) == 1
+    true_positives = torch.bitwise_and(gts, preds).sum(dim=0)
+    xor = torch.bitwise_xor(gts, preds)
+    false_positives = torch.bitwise_and(preds, xor).sum(dim=0)
+    false_negatives = torch.bitwise_and(gts, xor).sum(dim=0)
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / (true_positives + false_negatives)
+    f1 = 2 * precision * recall / (precision + recall)
+    # minimum, maximum = ({'precision': min(precision), 'recall': min(recall), 'f1': min(f1)},
+    #                     {'precision': max(precision), 'recall': max(recall), 'f1': max(f1)})
+    people, sketches, mask_ind = _init_heatmaps()
+    precision = {'adult': precision[:21], 'child': precision[21:]}
+    recall = {'adult': recall[:21], 'child': recall[21:]}
+    f1 = {'adult': f1[:21], 'child': f1[21:]}
+    for person in people:
+        for _set, name in [(precision, 'precision'), (recall, 'recall'), (f1, 'f1')]:
+            img_cpy = sketches[person].copy()
+            colormap = plt.get_cmap('RdYlGn')
+            for region, value in enumerate(_set[person]):
+                print(f'{person} - {name}: {value}')
+                # x = (value - minimum[name]) / (maximum[name] - minimum[name])
+                r, g, b = colormap(value)[:3]
+                img_cpy[mask_ind[person] == (region + 1)] = (255 * b, 255 * g, 255 * r)  # for cv2 the order is bgr
+            cv2.imwrite(os.path.join(save_dir, f'{person}_{name}.png'), img_cpy)
+    #     cv2.imshow(person, img_cpy)
+    # cv2.waitKey(0)
+
+
+def vis_threshold_eval(gts, scores, eval_func, epoch, save_dir, **kwargs):
+    os.makedirs(save_dir, exist_ok=True)
+    xs = torch.linspace(0, 1, 25)
+    for key in gts:
+        eval_results = [eval_func(gts[key], torch.sigmoid(torch.Tensor(scores[key])) > thresh, **kwargs) for thresh in xs]
+        plt.plot(xs, eval_results, label=key)
+    plt.xticks(torch.linspace(0, 1, 25))
+    plt.xticks(rotation=90)
+    plt.grid()
+    plt.legend()
+    # plt.show()
+    plt.savefig(os.path.join(save_dir, f'{epoch}.png'))
+    plt.clf()
+
