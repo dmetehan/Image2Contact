dbbd4989ebaadcf08a2cc7ca761371159d608211
M.Doyran <m.doyran@uu.nl>
2024-04-16 23:07:22 +0200

    modality cross validation results added

--- cross_validation.py
+++ cross_validation.py
@@ -7,7 +7,8 @@ import numpy as np
 def run_cross_val_loss():
     for i in range(6):
         for fold in range(1, 5):
-            cmd = ['venv/bin/python', 'YOUth_train.py', '/home/sac/GithubRepos/ContactClassification-ssd/YOUth10mSignatures/',
+            cmd = ['venv/bin/python', 'YOUth_train.py',
+                   '/home/sac/GithubRepos/ContactClassification-ssd/YOUth10mSignatures/',
                    f'configs/loss_weights/lw{i}_config.yaml', 'exp/YOUth_cross', f'{fold}', '--log_val_results']
             print(cmd)
             subprocess.Popen(cmd).wait()
@@ -17,20 +18,22 @@ def run_cross_val_loss():
 def run_cross_val_modality():
     for i in range(5):
         for fold in range(1, 5):
-            cmd = ['venv/bin/python', 'YOUth_train.py', '/home/sac/GithubRepos/ContactClassification-ssd/YOUth10mSignatures/',
+            cmd = ['venv/bin/python', 'YOUth_train.py',
+                   '/home/sac/GithubRepos/ContactClassification-ssd/YOUth10mSignatures/',
                    f'configs/modality/md{i}_config.yaml', 'exp/YOUth_cross', f'{fold}', '--log_val_results']
             print(cmd)
             subprocess.Popen(cmd).wait()
             time.sleep(10)  # Sleep for 10 seconds
 
 
-def analyze_results(results):
+def analyze_results_loss(results):
     ordered_keys = ['6+6', '6x6', '21+21', '21x21']
     ordered_keys_2 = ['12', '6x6', '42', '21x21']
     for key in ordered_keys:
         print(f"{key:^6s}", end='&\t')
-    for key in ordered_keys:
-        print(f"{key:^14s}", end='&\t')
+    for k, key in enumerate(ordered_keys):
+        print(f"{key:^14s}",
+              end=' &\t' if k < len(ordered_keys) - 1 else ' \\\\\t')
     print()
     for res in results:
         params = res[-1]
@@ -41,36 +44,72 @@ def analyze_results(results):
         # print(params)
         for key in ordered_keys_2:
             print(f"{f'{params[key]}':^6s}", end='&\t')
-        for key in ordered_keys_2:
-            print(f"{f'{np.mean(combined[key])*100:.2f}':>6s} ({np.std(combined[key])*100:.2f})", end=' &\t')
+        for k, key in enumerate(ordered_keys_2):
+            print(f"{f'{np.mean(combined[key]) * 100:.2f}':>6s} ({np.std(combined[key]) * 100:.2f})",
+                  end=' &\t' if k < len(ordered_keys_2) - 1 else ' \\\\''\t')
         print()
 
 
-def read_results(path):
-    # TODO: Needs to be updated with the new val_results
+def analyze_results_modality(results):
+    ordered_keys_meaningful = ['2D Pose Heatmaps', 'Cropped Image', 'Body Part Maps']
+    ordered_modalities = ['jointmaps', 'rgb', 'bodyparts']
+    ordered_keys = ['6+6', '6x6', '21+21', '21x21']
+    ordered_keys_2 = ['12', '6x6', '42', '21x21']
+    for key in ordered_keys_meaningful:
+        print(f"{key:^6s}", end='&\t')
+    for k, key in enumerate(ordered_keys):
+        print(f"{key:^14s}",
+              end=' &\t' if k < len(ordered_keys) - 1 else ' \\\\\t')
+    print()
+    for res in results:
+        modalities = res[-1].split('_')
+        combined = {key: [res[0][key]] for key in res[0] if key != "best_epoch"}
+        for one_result in res[1:-1]:
+            for key in combined:
+                combined[key].append(one_result[key])
+        checkmark = "\\checkmark"
+        for key in ordered_modalities:
+            print(f'{checkmark if key in modalities else "":^6s}', end='&\t')
+        for k, key in enumerate(ordered_keys_2):
+            print(f"{f'{np.mean(combined[key]) * 100:.2f}':>6s} ({np.std(combined[key]) * 100:.2f})",
+                  end=' &\t' if k < len(ordered_keys_2) - 1 else ' \\\\\t')
+        print()
+
+
+def read_results(path, is_loss=True):
     results = []
     with open(path) as f:
         for line in f:
             line = line.replace('*', 'x')
             w_start_i = line.index('[')
             w_end_i = line.index(']')
-            cur_res = json.loads(line[:w_start_i-2].replace('\'', '\"'))
+            cur_res = json.loads(line[:w_start_i - 2].replace('\'', '\"'))
             # DON'T CHANGE THE ORDER OF THE KEYS BELOW AS THIS IS HOW THE FILE IS WRITTEN
-            weights = {key: w for key, w in zip(['42', '12', '21x21', '6x6'], json.loads(line[w_start_i:w_end_i+1]))}
+            weights = {key: w for key, w in zip(['42', '12', '21x21', '6x6'], json.loads(line[w_start_i:w_end_i + 1]))}
             fold = int(line.strip()[-1])
             if fold == 1:
                 results.append([])
+
             if weights['21x21'] == 0:
                 results[-1].append(cur_res['6x6'])
             else:
                 results[-1].append(cur_res['21x21'])
+
             if fold == 4:
-                results[-1].append(weights)
+                if is_loss:
+                    results[-1].append(weights)
+                else:
+                    modality = line[w_end_i + 2:].split(',')[0].strip()
+                    results[-1].append(modality)
     return results
 
 
 if __name__ == '__main__':
     # run_cross_val_loss()
-    run_cross_val_modality()
-    # results = read_results("val_results.txt")
-    # analyze_results(results)
+    # run_cross_val_modality()
+
+    results = read_results("val_results_modality.txt", is_loss=False)
+    analyze_results_modality(results)
+
+    # results = read_results("val_results_loss.txt", is_loss=True)
+    # analyze_results_loss(results)
